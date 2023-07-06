c33d2f9155c35d6a9d43dad06a3c5c29fa162c1e
Gourav Sharma <103135156+Gourav-dark@users.noreply.github.com>
2023-07-06 18:12:56 +0530

    First

---
+++ MedicineX/Controllers/UsersController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedicineX.DBContext;
+using MedicineX.DbModel;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MedicineX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        //private readonly IConfiguration _config;
+
+        public UsersController(DatabaseContext context)
+        {
+            _context = context;
+            //_config = config;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutUser(int id, User user)
+        //{
+        //    if (id != user.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(user).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!UserExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'DatabaseContext.Users'  is null.");
+          }
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("Sucessfully");
+        }
+
+        // DELETE: api/Users/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteUser(int id)
+        //{
+        //    if (_context.Users == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var user = await _context.Users.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Users.Remove(user);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool UserExists(int id)
+        //{
+        //    return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        //}
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<ActionResult> LoginUser(Login user)
+        {
+            var exuser=_context.Users.FirstOrDefault(u=>u.Email==user.Email && u.Password==user.Password);
+            if(exuser == null)
+            {
+                return NotFound("user Not Found");
+            }
+            //string jwt = JwtTokenCreate(exuser.Email,exuser.Type,exuser.Id.ToString());
+
+            return Ok(exuser.Id);
+        }
+        //private string JwtTokenCreate(string email, string role, string userId)
+        //{
+        //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+        //    var credential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        //    var claims = new[]
+        //    {
+        //            new Claim("Email",email),
+        //            new Claim("Role",role),
+        //            new Claim("UserId",userId),
+        //            new Claim(ClaimTypes.Role,role),
+        //    };
+        //    var token = new JwtSecurityToken(
+        //        issuer: _config["JWT:Issuer"],
+        //        audience: _config["JWT:Audience"],
+        //        claims: claims,
+        //        expires: DateTime.Now.AddMinutes(180),
+        //        signingCredentials: credential
+        //        );
+        //    var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        //    return jwt;
+        //}
+    }
+}
